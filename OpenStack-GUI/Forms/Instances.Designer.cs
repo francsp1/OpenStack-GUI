7e6681fcf7a5bc04e48e190abe31952101769a5f
RuiSilva97 <80705932+RuiSilva97@users.noreply.github.com>
2022-04-01 17:30:14 +0100

    InstanceForm

---
+++ OpenStack-GUI/Forms/Instances.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace OpenStack_GUI.Forms
+{
+    partial class Instances
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Instances
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.MaximizeBox = false;
+            this.Name = "Instances";
+            this.Text = "Instances";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
