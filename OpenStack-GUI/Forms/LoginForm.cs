c5b35bae09974698ff6e2d2b5c349942e6c68cc1
Francisco Pedrosa <francsp1@hotmail.com>
2022-04-01 15:36:31 +0100

    Login

--- OpenStack-GUI/Forms/LoginForm.cs
+++ OpenStack-GUI/Forms/LoginForm.cs
@@ -20,10 +20,19 @@ namespace OpenStack_GUI.Forms
 
             materialSkinInitialize();
 
-            //txtDomain.Text = "192.168.100.140";
-            //txtPort.Text = "80";
-            //txtUsername.Text = "demo";
-            //txtPassword.Text = "secret";
+            /*
+            txtDomain.Text = "192.168.100.140";
+            txtPort.Text = "80";
+            txtUsername.Text = "demo";
+            txtPassword.Text = "secret";
+            */
+
+            /*
+            txtDomain.Text = "127.0.0.0.1";
+            txtPort.Text = "8080";
+            txtUsername.Text = "demo";
+            txtPassword.Text = "secret";
+            */
 
         }
 
