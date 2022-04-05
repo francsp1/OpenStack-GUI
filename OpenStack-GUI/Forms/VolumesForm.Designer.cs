df743a3a3f49252220912144b218cca929f2ad6c
RuiSilva97 <80705932+RuiSilva97@users.noreply.github.com>
2022-04-05 18:56:36 +0100

    GetVolumes

--- OpenStack-GUI/Forms/VolumesForm.Designer.cs
+++ OpenStack-GUI/Forms/VolumesForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace OpenStack_GUI
+namespace OpenStack_GUI.Forms
 {
     partial class VolumesForm
     {
@@ -32,12 +32,21 @@ namespace OpenStack_GUI
             this.volumesTabSelector = new MaterialSkin.Controls.MaterialTabSelector();
             this.volumesTabControl = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.volumesGridView = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnGroup = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnAvailabilityZone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnBootable = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnEncrypted = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.volumesTabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumesGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // volumesTabSelector
@@ -48,10 +57,10 @@ namespace OpenStack_GUI
             this.volumesTabSelector.Dock = System.Windows.Forms.DockStyle.Top;
             this.volumesTabSelector.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.volumesTabSelector.Location = new System.Drawing.Point(0, 0);
-            this.volumesTabSelector.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.volumesTabSelector.Margin = new System.Windows.Forms.Padding(2);
             this.volumesTabSelector.MouseState = MaterialSkin.MouseState.HOVER;
             this.volumesTabSelector.Name = "volumesTabSelector";
-            this.volumesTabSelector.Size = new System.Drawing.Size(1519, 48);
+            this.volumesTabSelector.Size = new System.Drawing.Size(1139, 39);
             this.volumesTabSelector.TabIndex = 0;
             this.volumesTabSelector.Text = "materialTabSelector1";
             // 
@@ -61,72 +70,126 @@ namespace OpenStack_GUI
             this.volumesTabControl.Controls.Add(this.tabPage2);
             this.volumesTabControl.Depth = 0;
             this.volumesTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.volumesTabControl.Location = new System.Drawing.Point(0, 48);
-            this.volumesTabControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.volumesTabControl.Location = new System.Drawing.Point(0, 39);
+            this.volumesTabControl.Margin = new System.Windows.Forms.Padding(2);
             this.volumesTabControl.MouseState = MaterialSkin.MouseState.HOVER;
             this.volumesTabControl.Multiline = true;
             this.volumesTabControl.Name = "volumesTabControl";
             this.volumesTabControl.SelectedIndex = 0;
-            this.volumesTabControl.Size = new System.Drawing.Size(1519, 668);
+            this.volumesTabControl.Size = new System.Drawing.Size(1139, 543);
             this.volumesTabControl.TabIndex = 1;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Controls.Add(this.volumesGridView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage1.Size = new System.Drawing.Size(1511, 639);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(1131, 517);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Volumes";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // volumesGridView
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ID});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.GridColor = System.Drawing.Color.White;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 2);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1505, 635);
-            this.dataGridView1.TabIndex = 0;
+            this.volumesGridView.BackgroundColor = System.Drawing.Color.White;
+            this.volumesGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.volumesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.volumesGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.columnName,
+            this.columnDescription,
+            this.columnSize,
+            this.columnStatus,
+            this.columnGroup,
+            this.columnType,
+            this.columnAvailabilityZone,
+            this.columnBootable,
+            this.columnEncrypted});
+            this.volumesGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.volumesGridView.GridColor = System.Drawing.Color.White;
+            this.volumesGridView.Location = new System.Drawing.Point(2, 2);
+            this.volumesGridView.Name = "volumesGridView";
+            this.volumesGridView.RowHeadersWidth = 51;
+            this.volumesGridView.Size = new System.Drawing.Size(1127, 513);
+            this.volumesGridView.TabIndex = 0;
+            this.volumesGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(1131, 517);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Create Volumes";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // ID
             // 
             this.ID.HeaderText = "ID";
             this.ID.MinimumWidth = 6;
             this.ID.Name = "ID";
-            this.ID.Width = 125;
+            this.ID.Visible = false;
+            this.ID.Width = 6;
             // 
-            // tabPage2
+            // columnName
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Size = new System.Drawing.Size(1511, 639);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Create Volumes";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.columnName.HeaderText = "Name";
+            this.columnName.Name = "columnName";
+            // 
+            // columnDescription
+            // 
+            this.columnDescription.HeaderText = "Description";
+            this.columnDescription.Name = "columnDescription";
+            // 
+            // columnSize
+            // 
+            this.columnSize.HeaderText = "Size";
+            this.columnSize.Name = "columnSize";
+            // 
+            // columnStatus
+            // 
+            this.columnStatus.HeaderText = "Status";
+            this.columnStatus.Name = "columnStatus";
+            // 
+            // columnGroup
+            // 
+            this.columnGroup.HeaderText = "Group";
+            this.columnGroup.Name = "columnGroup";
+            // 
+            // columnType
+            // 
+            this.columnType.HeaderText = "Type";
+            this.columnType.Name = "columnType";
+            // 
+            // columnAvailabilityZone
+            // 
+            this.columnAvailabilityZone.HeaderText = "Availability Zone";
+            this.columnAvailabilityZone.Name = "columnAvailabilityZone";
+            // 
+            // columnBootable
+            // 
+            this.columnBootable.HeaderText = "Bootable";
+            this.columnBootable.Name = "columnBootable";
+            // 
+            // columnEncrypted
+            // 
+            this.columnEncrypted.HeaderText = "Encrypted";
+            this.columnEncrypted.Name = "columnEncrypted";
             // 
             // VolumesForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1519, 716);
+            this.ClientSize = new System.Drawing.Size(1139, 582);
             this.ControlBox = false;
             this.Controls.Add(this.volumesTabControl);
             this.Controls.Add(this.volumesTabSelector);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "VolumesForm";
@@ -137,7 +200,7 @@ namespace OpenStack_GUI
             this.Load += new System.EventHandler(this.Volumes_Load);
             this.volumesTabControl.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volumesGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -148,7 +211,16 @@ namespace OpenStack_GUI
         private MaterialSkin.Controls.MaterialTabControl volumesTabControl;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView volumesGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnSize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnGroup;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnAvailabilityZone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnBootable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnEncrypted;
     }
 }
--- OpenStack-GUI/Forms/VolumesForm.cs
+++ OpenStack-GUI/Forms/VolumesForm.cs
@@ -1,25 +1,61 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using OpenStack_GUI.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OpenStack_GUI
+namespace OpenStack_GUI.Forms
 {
     public partial class VolumesForm : Form
     {
         public VolumesForm()
         {
             InitializeComponent();
+
+            try
+            {
+                WebClient myWebClient = new WebClient();
+
+                myWebClient.Headers.Add("x-auth-token", GlobalSessionDetails.ScopedToken);
+
+                string url = GlobalSessionDetails.Protocol + "://" + GlobalSessionDetails.Domain + ":" + GlobalSessionDetails.Port + "/volume/v3/" + GlobalSessionDetails.ProjectId + "/volumes/detail";
+                Console.WriteLine(url);
+                var responseString = myWebClient.DownloadString(url);
+
+                var myObject = JObject.Parse(responseString);
+                JArray volumes = (JArray)myObject["volumes"];
+
+                volumesGridView.Rows.Clear();
+                volumesGridView.Refresh();
+                for (int i = 0; i < volumes.Count; i++)
+                {
+
+                    var currentVolume = volumes[i];
+                    volumesGridView.Rows.Add(false, currentVolume["name"].ToString(), currentVolume["description"].ToString(), currentVolume["size"].ToString() + " GiB", currentVolume["status"].ToString(), currentVolume["metadata"].ToString(), currentVolume["volume_type"].ToString(), currentVolume["availability_zone"].ToString(), bool.Parse(currentVolume["bootable"].ToString()) ? "Yes" : "No", bool.Parse(currentVolume["encrypted"].ToString()) ? "Yes" : "No");
+                }
+                
+            }
+            catch (Exception excp)
+            {
+                MessageBox.Show(excp.Message, "Could not get the Volumes", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void Volumes_Load(object sender, EventArgs e)
         {
            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
