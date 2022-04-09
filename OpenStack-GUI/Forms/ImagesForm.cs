0fe8871840f609cc7f81b1d67ffee9cc4647b208
Francisco Pedrosa <francsp1@hotmail.com>
2022-04-09 01:46:27 +0100

    Delete image v5

--- OpenStack-GUI/Forms/ImagesForm.cs
+++ OpenStack-GUI/Forms/ImagesForm.cs
@@ -36,9 +36,10 @@ namespace OpenStack_GUI.Forms
         {
             imagesDataGridView.Rows.Clear();
             imagesDataGridView.Refresh();
-
+            /*
             using (WebClient myWebClient = new WebClient())
             {
+               
                 try
                 { 
                     var responseJsonObject = new JObject();
@@ -70,10 +71,43 @@ namespace OpenStack_GUI.Forms
                 {
                     MessageBox.Show(excp.Message, "Could not get the Images", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
-
             }
-                
+            */
+
+            string url = GlobalSessionDetails.Protocol + "://" + GlobalSessionDetails.Domain + ":" + GlobalSessionDetails.Port + "/image/v2/images";
+
+            var client = GlobalSessionDetails._clientFactory.CreateClient();
+
+            client.DefaultRequestHeaders.Add("X-Auth-Token", GlobalSessionDetails.ScopedToken);
+
+            var responseJsonObject = new JObject();
+            do
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+                HttpResponseMessage response = client.SendAsync(request).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show(response.ReasonPhrase, "Could not get the Images", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                responseJsonObject = JObject.Parse(responseString);
+                JArray images = (JArray)responseJsonObject["images"];
+
+                for (int i = 0; i < images.Count; i++)
+                {
+                    var currentImage = images[i];
+                    imagesDataGridView.Rows.Add(imagesDataGridView.Rows.Count + 1, false, currentImage["id"].ToString(), currentImage["owner"].ToString(), currentImage["name"].ToString(), currentImage["status"].ToString(), currentImage["visibility"].ToString(), bool.Parse(currentImage["protected"].ToString()) ? "Yes" : "No", currentImage["disk_format"].ToString(), currentImage["container_format"].ToString(), (((float)long.Parse(currentImage["size"].ToString()) / 1048576)).ToString("0.00") + "MB", "Delete");
+                }
+                if (responseJsonObject["next"] != null)
+                {
+                    url = GlobalSessionDetails.Protocol + "://" + GlobalSessionDetails.Domain + ":" + GlobalSessionDetails.Port + "/image" + responseJsonObject["next"].ToString();
+                }
+
+            } while (responseJsonObject["next"] != null);  // get the images while there is a next filed in the response 
+
         }
 
         private void btnCreateImage_Click(object sender, EventArgs e)
