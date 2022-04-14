9787423166ece60843bcc712f900951785775aba
RuiSilva97 <80705932+RuiSilva97@users.noreply.github.com>
2022-04-14 14:14:54 +0100

    IconsMain

--- OpenStack-GUI/Forms/MainForm.Designer.cs
+++ OpenStack-GUI/Forms/MainForm.Designer.cs
@@ -30,16 +30,17 @@ namespace OpenStack_GUI.Forms
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.mainTabControl = new MaterialSkin.Controls.MaterialTabControl();
             this.ImageServiceTab = new System.Windows.Forms.TabPage();
             this.panelImageService = new System.Windows.Forms.Panel();
             this.volumesTab = new System.Windows.Forms.TabPage();
             this.panelVolumes = new System.Windows.Forms.Panel();
+            this.instanceTab = new System.Windows.Forms.TabPage();
             this.mainTabControlImageList = new System.Windows.Forms.ImageList(this.components);
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.comboBoxProjects = new System.Windows.Forms.ComboBox();
             this.btnLogout = new MaterialSkin.Controls.MaterialButton();
-            this.instanceTab = new System.Windows.Forms.TabPage();
             this.mainTabControl.SuspendLayout();
             this.ImageServiceTab.SuspendLayout();
             this.volumesTab.SuspendLayout();
@@ -54,7 +55,7 @@ namespace OpenStack_GUI.Forms
             this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainTabControl.ImageList = this.mainTabControlImageList;
             this.mainTabControl.Location = new System.Drawing.Point(2, 52);
-            this.mainTabControl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mainTabControl.Margin = new System.Windows.Forms.Padding(2);
             this.mainTabControl.MouseState = MaterialSkin.MouseState.HOVER;
             this.mainTabControl.Multiline = true;
             this.mainTabControl.Name = "mainTabControl";
@@ -66,10 +67,11 @@ namespace OpenStack_GUI.Forms
             // 
             this.ImageServiceTab.BackColor = System.Drawing.Color.White;
             this.ImageServiceTab.Controls.Add(this.panelImageService);
+            this.ImageServiceTab.ImageKey = "image.png";
             this.ImageServiceTab.Location = new System.Drawing.Point(4, 39);
-            this.ImageServiceTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ImageServiceTab.Margin = new System.Windows.Forms.Padding(2);
             this.ImageServiceTab.Name = "ImageServiceTab";
-            this.ImageServiceTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ImageServiceTab.Padding = new System.Windows.Forms.Padding(2);
             this.ImageServiceTab.Size = new System.Drawing.Size(1155, 621);
             this.ImageServiceTab.TabIndex = 0;
             this.ImageServiceTab.Text = "Image Service";
@@ -79,19 +81,20 @@ namespace OpenStack_GUI.Forms
             this.panelImageService.BackColor = System.Drawing.Color.White;
             this.panelImageService.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelImageService.Location = new System.Drawing.Point(2, 2);
-            this.panelImageService.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelImageService.Margin = new System.Windows.Forms.Padding(2);
             this.panelImageService.Name = "panelImageService";
             this.panelImageService.Size = new System.Drawing.Size(1151, 617);
             this.panelImageService.TabIndex = 0;
             // 
             // volumesTab
             // 
-            this.volumesTab.BackColor = System.Drawing.Color.White;
+            this.volumesTab.BackColor = System.Drawing.Color.Transparent;
             this.volumesTab.Controls.Add(this.panelVolumes);
+            this.volumesTab.ImageKey = "hard-drive.png";
             this.volumesTab.Location = new System.Drawing.Point(4, 39);
-            this.volumesTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.volumesTab.Margin = new System.Windows.Forms.Padding(2);
             this.volumesTab.Name = "volumesTab";
-            this.volumesTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.volumesTab.Padding = new System.Windows.Forms.Padding(2);
             this.volumesTab.Size = new System.Drawing.Size(1155, 621);
             this.volumesTab.TabIndex = 1;
             this.volumesTab.Text = "Volumes";
@@ -104,11 +107,24 @@ namespace OpenStack_GUI.Forms
             this.panelVolumes.Size = new System.Drawing.Size(1151, 617);
             this.panelVolumes.TabIndex = 0;
             // 
+            // instanceTab
+            // 
+            this.instanceTab.BackColor = System.Drawing.Color.White;
+            this.instanceTab.ImageIndex = 3;
+            this.instanceTab.Location = new System.Drawing.Point(4, 39);
+            this.instanceTab.Name = "instanceTab";
+            this.instanceTab.Size = new System.Drawing.Size(1155, 621);
+            this.instanceTab.TabIndex = 2;
+            this.instanceTab.Text = "Instances";
+            // 
             // mainTabControlImageList
             // 
-            this.mainTabControlImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
-            this.mainTabControlImageList.ImageSize = new System.Drawing.Size(32, 32);
-            this.mainTabControlImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.mainTabControlImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("mainTabControlImageList.ImageStream")));
+            this.mainTabControlImageList.TransparentColor = System.Drawing.Color.White;
+            this.mainTabControlImageList.Images.SetKeyName(0, "image.png");
+            this.mainTabControlImageList.Images.SetKeyName(1, "database.png");
+            this.mainTabControlImageList.Images.SetKeyName(2, "hard-drive.png");
+            this.mainTabControlImageList.Images.SetKeyName(3, "data-server.png");
             // 
             // imageList1
             // 
@@ -121,7 +137,7 @@ namespace OpenStack_GUI.Forms
             this.comboBoxProjects.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxProjects.FormattingEnabled = true;
             this.comboBoxProjects.Location = new System.Drawing.Point(784, 32);
-            this.comboBoxProjects.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxProjects.Margin = new System.Windows.Forms.Padding(2);
             this.comboBoxProjects.Name = "comboBoxProjects";
             this.comboBoxProjects.Size = new System.Drawing.Size(192, 21);
             this.comboBoxProjects.TabIndex = 1;
@@ -146,15 +162,6 @@ namespace OpenStack_GUI.Forms
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogOut_Click);
             // 
-            // instanceTab
-            // 
-            this.instanceTab.Location = new System.Drawing.Point(4, 39);
-            this.instanceTab.Name = "instanceTab";
-            this.instanceTab.Size = new System.Drawing.Size(1155, 621);
-            this.instanceTab.TabIndex = 2;
-            this.instanceTab.Text = "Instances";
-            this.instanceTab.UseVisualStyleBackColor = true;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -165,7 +172,7 @@ namespace OpenStack_GUI.Forms
             this.Controls.Add(this.mainTabControl);
             this.DrawerShowIconsWhenHidden = true;
             this.DrawerTabControl = this.mainTabControl;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Padding = new System.Windows.Forms.Padding(2, 52, 2, 2);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
